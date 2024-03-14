399a00d5d4701be7572c923c121c882a6459344d
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-03-15 06:42:23 +0900

    init

---
+++ src/test/java/org/jpabook/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpabook.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
