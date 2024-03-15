0bbabac5f9a3dd0ed3b98ca4a2433d11de916e7e
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-03-15 21:54:39 +0900

    querydsl 기본

---
+++ src/test/java/org/jpabook/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,68 @@
+package org.jpabook.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.jpabook.querydsl.entity.Member;
+import org.jpabook.querydsl.entity.QMember;
+import org.jpabook.querydsl.entity.Team;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() throws Exception {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() throws Exception {
+        String queryString = "SELECT m FROM Member m WHERE m.username = :username";
+        Member findMember = em.createQuery(queryString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertEquals(findMember.getUsername(), "member1");
+    }
+
+    @Test
+    public void startQuerydsl() throws Exception {
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertEquals(findMember.getUsername(), "member1");
+    }
+}
